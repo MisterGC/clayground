0224fafef68e99afb66896457ebf71423e28cd33
MisterGC <serein.pfeiffer@gmail.com>
2019-04-10 22:59:30 +0200

    Add command line param that allows specifying the dir which contains dynamically changing QML files.

--- demo/main.cpp
+++ demo/main.cpp
@@ -10,14 +10,26 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
+    QCoreApplication::setApplicationName("Qml LiveLoader");
+    QCoreApplication::setApplicationVersion("0.1");
+
+    QCommandLineParser parser;
+    QCommandLineOption opt("dynqmldir",
+                           "Sets the directory that contains dynamic qml.",
+                           "directory",
+                           "<working directory>");
+    parser.addOption(opt);
+    parser.process(app);
+    auto dynQmlDir = QDir::currentPath();
+    if (parser.isSet("dynqmldir"))
+        dynQmlDir = parser.value("dynqmldir");
 
     QmlEngineWrapper engine;
-    QmlFileObserver watcher(QDir::currentPath() + "/qml");
+    QmlFileObserver watcher(dynQmlDir);
     engine.rootContext()->setContextProperty("FileObserver", &watcher);
     engine.rootContext()->setContextProperty("QmlCache", &engine);
-    engine.addImportPath(QDir::currentPath() + "/qml");
+    engine.addImportPath(dynQmlDir);
 
     engine.load(QUrl("qrc:/main.qml"));
     if (engine.rootObjects().isEmpty()) return -1;
