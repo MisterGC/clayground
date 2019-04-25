1a19bc26a1111216dda1c6d705e845e022b89e35
MisterGC <serein.pfeiffer@gmail.com>
2019-04-26 00:39:32 +0200

    Populator: Use it in the Gui SBX and SVG is basically parsed - to be refined.

--- sandboxes/gui/Sandbox.qml
+++ sandboxes/gui/Sandbox.qml
@@ -1,10 +1,19 @@
 import QtQuick 2.12
 import QtQuick.Controls 2.5
 import "qrc:/" as LivLd
+import GcPopulator 1.0
 
 Rectangle
 {
     color: "grey"
+    Populator
+    {
+        Component.onCompleted: loadSvgPopulationModel("/home/mistergc/dev/qml_live_loader/sandboxes/populator/sample_level.svg")
+        onCreateItemAt: {
+            console.log("Create item: " + componentName + " x: " + x + " y: " + y);
+        }
+    }
+
     Column
     {
         spacing: 10
--- sandboxes/populator/populator.cpp
+++ sandboxes/populator/populator.cpp
@@ -1,6 +1,47 @@
 #include "populator.h"
+#include <QFile>
+#include <QXmlStreamReader>
+#include <QDebug>
 
 void Populator::loadSvgPopulationModel(const QString &pathToSvg)
 {
-    emit createItemAt("Test.qml", 20, 10);
+    QFile xmlFile(pathToSvg);
+    if (!xmlFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qCritical() << "Error unable to open " << pathToSvg;
+        return;
+    }
+    QXmlStreamReader xmlReader(&xmlFile);
+
+    bool readingMapContent = false;
+    while(!xmlReader.atEnd() && !xmlReader.hasError())
+    {
+        auto token = xmlReader.readNext();
+        if(token == QXmlStreamReader::StartElement)
+        {
+            auto nam = xmlReader.name();
+            if(nam == "g") {
+                auto attribs = xmlReader.attributes();
+                auto lbl = attribs.value("inkscape:label").toString();
+                if (lbl == "Map") readingMapContent = true;
+                else if (lbl == "Legend") readingMapContent = false;
+            }
+            else if (readingMapContent && nam == "rect")
+            {
+                auto attribs = xmlReader.attributes();
+                auto x = attribs.value("x").toFloat();
+                auto y = attribs.value("y").toFloat();
+                auto comp = attribs.value("id").toString();
+                // TODO Convert SVG coords to pixel coords
+                emit createItemAt(comp, (int)x, (int)y);
+            }
+        }
+    }
+
+    if(xmlReader.hasError())
+    {
+        qCritical() << "Error while processing XML: " << xmlReader.errorString();
+    }
+
+    xmlReader.clear();
+    xmlFile.close();
 }
