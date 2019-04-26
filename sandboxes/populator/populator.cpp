93bf7f424e22024b290263577d98f5c826e46d72
MisterGC <serein.pfeiffer@gmail.com>
2019-04-26 22:34:31 +0200

    Populator: Observe last set svg and re-populate when svg changes.

--- sandboxes/populator/populator.cpp
+++ sandboxes/populator/populator.cpp
@@ -3,45 +3,81 @@
 #include <QXmlStreamReader>
 #include <QDebug>
 
-void Populator::loadSvgPopulationModel(const QString &pathToSvg)
+void Populator::onSvgChanged(const QString &path)
 {
-    QFile xmlFile(pathToSvg);
-    if (!xmlFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qCritical() << "Error unable to open " << pathToSvg;
-        return;
-    }
-    QXmlStreamReader xmlReader(&xmlFile);
+    // INFO Re-add file as otherwise (at least on Linux)
+    // further changes are not recognized
+    svgObserver_.removePath(path);
+    setPopulationModel(path);
+    svgObserver_.addPath(path);
+    syncWithSvg();
+}
 
-    bool readingMapContent = false;
-    while(!xmlReader.atEnd() && !xmlReader.hasError())
+void Populator::syncWithSvg()
+{
+    if (!svgObserver_.files().empty())
     {
-        auto token = xmlReader.readNext();
-        if(token == QXmlStreamReader::StartElement)
+        auto pathToSvg = svgObserver_.files().first();
+
+        QFile xmlFile(pathToSvg);
+        if (!xmlFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            qCritical() << "Error unable to open " << pathToSvg;
+            return;
+        }
+        QXmlStreamReader xmlReader(&xmlFile);
+
+        bool readingMapContent = false;
+        while(!xmlReader.atEnd() && !xmlReader.hasError())
         {
-            auto nam = xmlReader.name();
-            if(nam == "g") {
-                auto attribs = xmlReader.attributes();
-                auto lbl = attribs.value("inkscape:label").toString();
-                if (lbl == "Map") readingMapContent = true;
-                else if (lbl == "Legend") readingMapContent = false;
-            }
-            else if (readingMapContent && nam == "rect")
+            auto token = xmlReader.readNext();
+            if(token == QXmlStreamReader::StartElement)
             {
-                auto attribs = xmlReader.attributes();
-                auto x = attribs.value("x").toFloat();
-                auto y = attribs.value("y").toFloat();
-                auto comp = attribs.value("id").toString();
-                // TODO Convert SVG coords to pixel coords
-                emit createItemAt(comp, (int)x, (int)y);
+                auto nam = xmlReader.name();
+                if (nam == "svg") {
+                    auto attribs = xmlReader.attributes();
+                    auto widthPx = attribs.value("width").toInt();
+                    auto heightPx = attribs.value("height").toInt();
+                    auto viewBox = attribs.value("viewBox").toString().split(" ");
+                    auto widthWu = viewBox[2].toFloat();
+                    auto heightWu = viewBox[3].toFloat();
+                    emit aboutToPopulate(widthWu, heightWu, widthPx, heightPx);
+                }
+                else if (nam == "g") {
+                    auto attribs = xmlReader.attributes();
+                    auto lbl = attribs.value("inkscape:label").toString();
+                    if (lbl == "Map") readingMapContent = true;
+                    else if (lbl == "Legend") readingMapContent = false;
+                }
+                else if (readingMapContent && nam == "rect")
+                {
+                    // TODO use upper left instead of lower right
+                    auto attribs = xmlReader.attributes();
+                    auto x = attribs.value("x").toFloat();
+                    auto y = attribs.value("y").toFloat();
+                    auto comp = attribs.value("id").toString();
+                    qDebug() << "xSvg: " << x << " ySvg: " << y;
+                    emit createItemAt(comp, x, y);
+                }
             }
         }
-    }
 
-    if(xmlReader.hasError())
-    {
-        qCritical() << "Error while processing XML: " << xmlReader.errorString();
-    }
+        if(xmlReader.hasError())
+        {
+            qCritical() << "Error while processing XML: " << xmlReader.errorString();
+        }
 
-    xmlReader.clear();
-    xmlFile.close();
+        xmlReader.clear();
+        xmlFile.close();
+
+    }
+}
+
+void Populator::setPopulationModel(const QString &pathToSvg)
+{
+    connect(&svgObserver_, &QFileSystemWatcher::fileChanged,
+            this, &Populator::onSvgChanged,
+            static_cast<Qt::ConnectionType>(Qt::AutoConnection | Qt::UniqueConnection));
+    svgObserver_.removePaths(svgObserver_.files());
+    svgObserver_.addPath(pathToSvg);
+    syncWithSvg();
 }
--- sandboxes/populator/populator.h
+++ sandboxes/populator/populator.h
@@ -1,14 +1,26 @@
 #ifndef POPULATOR_H
 #define POPULATOR_H
 #include <QObject>
+#include <QFileSystemWatcher>
 
 class Populator: public QObject
 {
     Q_OBJECT
 
 public slots:
-    void loadSvgPopulationModel(const QString& pathToSvg);
+    void setPopulationModel(const QString& pathToSvg);
+
 signals:
-    void createItemAt(const QString& componentName, int x, int y);
+    void aboutToPopulate(float widthWu, float heightWu, int widthPx, int heightPx);
+    void createItemAt(const QString& componentName, float xWu, float yWu);
+
+private slots:
+    void onSvgChanged(const QString &path);
+
+private:
+    void syncWithSvg();
+
+private:
+    QFileSystemWatcher svgObserver_;
 };
 #endif
