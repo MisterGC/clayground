52c94048072e1e2d676bfe9a34c226f3e229ffed
Serein Pfeiffer <serein.pfeiffer@gmail.com>
2020-04-12 16:50:19 +0200

    Physics: Fixup of PolyBody type registration.

--- plugins/physics/physicsplugin.cpp
+++ plugins/physics/physicsplugin.cpp
@@ -29,5 +29,5 @@ void PhysicsPlugin::registerTypes(const char* uri)
 {
     qmlRegisterType(QUrl("qrc:/clayground/PhysicsItem.qml"),uri, 1,0,"PhysicsItem");
     qmlRegisterType(QUrl("qrc:/clayground/VisualizedBoxBody.qml"),uri, 1,0,"VisualizedBoxBody");
-    qmlRegisterType(QUrl("qrc:/clayground/VisualizedPolyBody.qml"),uri, 1,0,"VisualizedBoxBody");
+    qmlRegisterType(QUrl("qrc:/clayground/VisualizedPolyBody.qml"),uri, 1,0,"VisualizedPolyBody");
 }
