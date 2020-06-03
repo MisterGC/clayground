0353c666d01a5137da399f2497e3daa4194424a7
Serein Pfeiffer <serein.pfeiffer@gmail.com>
2020-06-03 17:34:53 +0200

    ctest: First PoC using QPA=offscreen to test sbx.

--- sandboxes/gui/main.cpp
+++ sandboxes/gui/main.cpp
@@ -2,12 +2,24 @@
 
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QDebug>
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
+    if (QGuiApplication::platformName() == "offscreen") {
+        QObject::connect(&engine,
+                         &QQmlApplicationEngine::warnings,
+                         [=] (const QList<QQmlError>& warnings) {
+            for (auto& w: warnings) qCritical() << w.toString();
+            exit(1);
+        }
+        );
+    }
     engine.load(QUrl("qrc:/main.qml"));
     return app.exec();
 }
