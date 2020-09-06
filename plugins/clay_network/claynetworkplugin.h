8b2e9bd76c6791e0435603851ac4535b9a208044
fernando <tonon.fernando@hotmail.com>
2020-09-06 14:06:33 -0400

    Intermediate state

--- plugins/clay_network/claynetworkplugin.h
+++ plugins/clay_network/claynetworkplugin.h
@@ -7,7 +7,7 @@
 class ClayNetworkPlugin: public QQmlExtensionPlugin
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "clayground.networkplugin")
+    Q_PLUGIN_METADATA(IID "com.storytelling-turtle.ClayNetworkPlugin")
 
 public:
     void registerTypes(const char* uri) override;
