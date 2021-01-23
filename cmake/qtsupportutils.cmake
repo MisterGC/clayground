87144305473ae720ec19147ad5814d1d519a7240
Serein Pfeiffer <s.pfeiffer@klebert-engineering.de>
2021-01-23 19:18:20 +0100

    cmake: Add macro that allows fetching the Qt version.

--- cmake/qtsupportutils.cmake
+++ cmake/qtsupportutils.cmake
@@ -6,3 +6,7 @@ macro(import_qt_components)
         find_package(Qt5 5.15 COMPONENTS ${ARGN} REQUIRED)
     endif()
 endmacro()
+
+macro(fetch_qt_version)
+    find_package(QT NAMES Qt6 Qt5 COMPONENTS Core REQUIRED)
+endmacro()
